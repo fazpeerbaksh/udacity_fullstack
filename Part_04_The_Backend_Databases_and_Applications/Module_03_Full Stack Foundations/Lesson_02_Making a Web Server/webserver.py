82f575561d1e4a1fa7ae5a3cc2d2ebe8c28fb249
fazpeerbaksh <fazkratos@gmail.com>
2021-07-18 16:15:29 +0530

    04-03-02 04. Building a Server with HTTPBaseServer

---
+++ Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_02_Making a Web Server/webserver.py
@@ -0,0 +1,29 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+class WebserverHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.endswith("/hello"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            message = ""
+            message += "<html><body>Hello!</body></html>"
+            self.wfile.write(message)
+            print(message)
+            return
+        else:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+def main():
+    try:
+        PORT = 8080
+        server = HTTPServer(('', PORT), WebserverHandler)
+        print("Server running running on port %s" % PORT)
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print("^C entered stopping server....")
+        server.socket.close()
+
+if __name__ == '__main__':
+    main()
