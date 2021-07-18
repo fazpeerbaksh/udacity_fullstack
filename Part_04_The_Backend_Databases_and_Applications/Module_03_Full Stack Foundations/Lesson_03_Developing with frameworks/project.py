eb0334104f22d83afcdbd1217f8e86e3c59fce47
fazpeerbaksh <fazkratos@gmail.com>
2021-07-18 20:25:24 +0530

    04-03-03 10. Templates Quiz

--- Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_03_Developing with frameworks/project.py
+++ Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_03_Developing with frameworks/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -13,7 +13,7 @@ session = DBSession()
 
 
 @app.route('/')
-@app.route('/hello')
+@app.route('/restaurants/')
 def HelloWorld():
     restaurant = session.query(Restaurant).first()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
@@ -27,6 +27,12 @@ def HelloWorld():
         output += '</br>'
         output += '</br>'
     return output
+
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    return render_template('menu.html', restaurant = restaurant, items = items)
 
 @app.route('/restaurants/<int:restaurant_id>/new/')
 def newMenuItem(restaurant_id):
