6ecbdd35a147519bd0c3c73ca0327c1c3d9e0fe6
fazpeerbaksh <fazkratos@gmail.com>
2021-07-18 21:35:28 +0530

    04-03-03 14. Delete Menu Item

--- Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_03_Developing with frameworks/project.py
+++ Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_03_Developing with frameworks/project.py
@@ -51,9 +51,15 @@ def editMenuItem(restaurant_id, menu_id):
         return render_template('editmenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = editedItem)
 	
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a new menu item"
+    itemToDelete = session.query(MenuItem).filter_by(restaurant_id = restaurant_id, id = menu_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+    else:
+        return render_template('deletemenuitem.html', restaurant_id = restaurant_id, item = itemToDelete)
 
 if __name__ == '__main__':
     app.debug = True
