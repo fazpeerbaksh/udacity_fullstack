ac6c0d8057c66a8affb010b3f259c02d580ea595
fazpeerbaksh <fazkratos@gmail.com>
2021-07-18 22:08:41 +0530

    04-03-03 18. Responding With JSON

--- Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_03_Developing with frameworks/project.py
+++ Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_03_Developing with frameworks/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -28,7 +28,14 @@ def HelloWorld():
         output += '</br>'
     return output
 
-@app.route('/restaurants/<int:restaurant_id>/')
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+@app.route('/restaurants/<int:restaurant_id>/menu')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
