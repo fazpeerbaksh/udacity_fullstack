a3f664bd39e4ca1a55dbc32ca8e8194b3a6b993a
fazpeerbaksh <fazkratos@gmail.com>
2021-07-18 22:14:16 +0530

    04-03-03 19. JSON Quiz

--- Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_03_Developing with frameworks/project.py
+++ Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_03_Developing with frameworks/project.py
@@ -35,6 +35,12 @@ def restaurantMenuJSON(restaurant_id):
         restaurant_id=restaurant_id).all()
     return jsonify(MenuItems=[i.serialize for i in items])
 
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menuItemJSON(restaurant_id, menu_id):
+    item = session.query(MenuItem).filter_by(restaurant_id=restaurant_id, id=menu_id).one()
+    return jsonify(MenuItem=item.serialize)
+
+
 @app.route('/restaurants/<int:restaurant_id>/menu')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
