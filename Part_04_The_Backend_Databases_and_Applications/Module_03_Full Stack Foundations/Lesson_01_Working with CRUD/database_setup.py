89cd9226cf4d57b0072cfd180f5e0c63eaf9c9af
fazpeerbaksh <fazkratos@gmail.com>
2021-07-18 13:55:09 +0530

    04-03-01 16. Creating a Database - Class and Table

--- Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_01_Working with CRUD/database_setup.py
+++ Part_04_The_Backend_Databases_and_Applications/Module_03_Full Stack Foundations/Lesson_01_Working with CRUD/database_setup.py
@@ -10,8 +10,12 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
 
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
 
+############## at the end ##############
 engine = create_engine('sqlite:///reataurantmenu.db')
-
 Base.metadata.create_all(engine)
